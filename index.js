a3ae0e18b4d41ad48eb5271684a04a7da7427a65
qingfengtommy <52116238+qingfengtommy@users.noreply.github.com>
2023-02-24 08:15:59 +0800

    update js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
+
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
 app.get('/', function (req, res) {
     res.render('home')
 })
+
+app.get('/characters', async (req, res) => {
+    try {
+        const response = await axios.get("https://swapi.dev/api/people")
+        res.render('characters', { characters: response.data })
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+)
 
 app.listen(3000, () => {
     console.log("listening on port 3000")
